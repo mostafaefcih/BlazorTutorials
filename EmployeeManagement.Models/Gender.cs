d2878766321d0404f5fdf735b8832634c6e526ba
Mostafa-Eid <Mostafa-Eid@DESKTOP-6MU528G>
2021-01-11 18:22:55 +0200

    creating model project creating Blazor Project

---
+++ EmployeeManagement.Models/Gender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeManagement.Models
+{
+    // Gender Enum
+    public enum Gender
+    {
+        Male,
+        Female,
+        Other
+    }
+}
