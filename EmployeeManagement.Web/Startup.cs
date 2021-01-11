a42a53960079cb707529f22a7e9d876ed35c3dc5
Mostafa-Eid <Mostafa-Eid@DESKTOP-6MU528G>
2021-01-11 18:23:16 +0200

    remove unused code from startup

--- EmployeeManagement.Web/Startup.cs
+++ EmployeeManagement.Web/Startup.cs
@@ -1,4 +1,4 @@
-using EmployeeManagement.Web.Data;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -28,7 +28,7 @@ namespace EmployeeManagement.Web
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            //services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
