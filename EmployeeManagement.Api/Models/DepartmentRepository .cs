2b9833eb8bf945ad34b73edf5698bc4432527f7d
Mostafa-Eid <Mostafa-Eid@DESKTOP-6MU528G>
2021-01-11 21:05:39 +0200

    add emp and dept repo add entity framwork

---
+++ EmployeeManagement.Api/Models/DepartmentRepository .cs
@@ -0,0 +1,29 @@
+﻿using EmployeeManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Api.Models
+{
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly AppDbContext appDbContext;
+
+        public DepartmentRepository(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public Department GetDepartment(int departmentId)
+        {
+            return appDbContext.Departments
+                .FirstOrDefault(d => d.DepartmentId == departmentId);
+        }
+
+        public IEnumerable<Department> GetDepartments()
+        {
+            return appDbContext.Departments;
+        }
+    }
+}
---
+++ EmployeeManagement.Web/Pages/EmployeeListBase.cs
@@ -0,0 +1,72 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class EmployeeListBase:ComponentBase
+    {
+        public IEnumerable<Employee> Employees { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            LoadEmployees();
+            return base.OnInitializedAsync();
+        }
+        private void LoadEmployees()
+        {
+            Employee e1 = new Employee
+            {
+                EmployeeId = 1,
+                FirstName = "John",
+                LastName = "Hastings",
+                Email = "David@pragimtech.com",
+                DateOfBrith = new DateTime(1980, 10, 5),
+                Gender = Gender.Male,
+                DepartmentId =  1,
+                PhotoPath = "images/john.png"
+            };
+
+            Employee e2 = new Employee
+            {
+                EmployeeId = 2,
+                FirstName = "Sam",
+                LastName = "Galloway",
+                Email = "Sam@pragimtech.com",
+                DateOfBrith = new DateTime(1981, 12, 22),
+                Gender = Gender.Male,
+                DepartmentId =    2,  
+                PhotoPath = "images/sam.jpg"
+            };
+
+            Employee e3 = new Employee
+            {
+                EmployeeId = 3,
+                FirstName = "Mary",
+                LastName = "Smith",
+                Email = "mary@pragimtech.com",
+                DateOfBrith = new DateTime(1979, 11, 11),
+                Gender = Gender.Female,
+                DepartmentId =   1 ,
+                PhotoPath = "images/mary.png"
+            };
+
+            Employee e4 = new Employee
+            {
+                EmployeeId = 3,
+                FirstName = "Sara",
+                LastName = "Longway",
+                Email = "sara@pragimtech.com",
+                DateOfBrith = new DateTime(1982, 9, 23),
+                Gender = Gender.Female,
+                DepartmentId =  3,  
+                PhotoPath = "images/sara.png"
+            };
+
+            Employees = new List<Employee> { e1, e2, e3, e4 };
+        }
+    }
+}
