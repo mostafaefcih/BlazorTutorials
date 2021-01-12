dd23bd68e2f21f74074ef5d7da504a5370b0c7a8
Mostafa-Eid <Mostafa-Eid@DESKTOP-6MU528G>
2021-01-12 18:02:39 +0200

    working with events

--- EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
+++ EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
@@ -1,6 +1,7 @@
 ﻿using EmployeeManagement.Models;
 using EmployeeManagement.Web.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,10 +16,30 @@ namespace EmployeeManagement.Web.Pages
         [Parameter]
         public string Id { get; set; }
         public Employee Employee { get; set; } = new Employee();
+        protected string Coordinates { get; set; }
+        protected string ButtonText { get; set; } = "Hide Footer";
+        protected string CssClass { get; set; } = null;
         protected async override Task OnInitializedAsync()
         {
             Id = Id ?? "1";
             Employee = await EmployeeService.GetEmployee(int.Parse(Id));
+        }
+        protected void Mouse_Move(MouseEventArgs e)
+        {
+            Coordinates = $"X = {e.ClientX } Y = {e.ClientY}";
+        }
+        protected void Button_Click()
+        {
+            if (ButtonText == "Hide Footer")
+            {
+                ButtonText = "Show Footer";
+                CssClass = "HideFooter";
+            }
+            else
+            {
+                CssClass = null;
+                ButtonText = "Hide Footer";
+            }
         }
 
     }
