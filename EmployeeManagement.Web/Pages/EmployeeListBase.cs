84c1d878a17fbe4e87c324d3eca4a6bd83cb7cba
Mostafa-Eid <Mostafa-Eid@DESKTOP-6MU528G>
2021-01-25 17:16:20 +0200

    display list as grid

--- EmployeeManagement.Web/Pages/EmployeeListBase.cs
+++ EmployeeManagement.Web/Pages/EmployeeListBase.cs
@@ -14,14 +14,18 @@ namespace EmployeeManagement.Web.Pages
         public IEmployeeService EmployeeService { get; set; }
 
         public IEnumerable<Employee> Employees { get; set; }
-        public bool ShowFooter { get; set; }
+        public IEnumerable<Employee> SearchedEmployees { get; set; } = new List<Employee>();
+        public bool ShowFooter { get; set; } = true;
         protected int SelectedEmployeesCount { get; set; } = 0;
         protected override async Task OnInitializedAsync()
         {
              Employees = (await EmployeeService.GetEmployees()).ToList();
+            SearchedEmployees = Employees;
             //return base.OnInitializedAsync();
         }
-      protected void  EmployeeSelectionChanged(bool isSelected) {
+        public Gender selectedGender { get; set; } = Gender.Female;
+        public string SelectedName { get; set; }
+        protected void  EmployeeSelectionChanged(bool isSelected) {
             if (isSelected) {
                 SelectedEmployeesCount++;
             } else {
@@ -31,6 +35,21 @@ namespace EmployeeManagement.Web.Pages
 
         protected async Task EmployeeDeleted() {
             Employees = (await EmployeeService.GetEmployees()).ToList();
+            SearchedEmployees = Employees;
+        }
+        public void SearchByGender(ChangeEventArgs  e) 
+        {
+            int value = (int)Enum.Parse(typeof(Gender), e.Value.ToString());
+            selectedGender = (Gender)value;
+            if (e.Value.ToString() == "-1")
+            {
+                SearchedEmployees = Employees;
+            }
+            else
+            {
+                SearchedEmployees = Employees.Where(e => e.Gender == selectedGender);
+
+            }
         }
     }
 }
---
+++ EmployeeManagement.Web/wwwroot/js/JsInterop.js
@@ -0,0 +1,21 @@
+﻿function CreateAlert() {
+    alert("Welcom to js from Blazor")
+}
+function CreatePrompte(question) {
+
+    return prompt(question)
+}
+function setElementTextById(id, text) {
+    document.getElementById(id).innerText = text;
+
+}
+function SetElementFocus(element) {
+    element.focus()
+}
+function randomize(maxNumber) {
+    DotNet.invokeMethodAsync('EmployeeManagement.Web', 'Randomize', maxNumber)
+        .then(result => {
+            setElementTextById("randomNumber", result)
+        })
+        ;
+}
