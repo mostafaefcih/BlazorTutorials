cc936565d8898e804816a2144e10f6996ab8f97b
Mostafa-Eid <Mostafa-Eid@DESKTOP-6MU528G>
2021-02-22 13:09:09 +0200

    completing Charts using CharJS

---
+++ EmployeeManagement.Web/wwwroot/js/Chart.js
@@ -0,0 +1,58 @@
+function CharIt(departments, EmpsPerDepratment) {
+    let myChart = document.getElementById('Chart').getContext('2d');
+    console.log("emp data ", departments, EmpsPerDepratment);
+    // Global Options
+    Chart.defaults.global.defaultFontFamily = 'Lato';
+    Chart.defaults.global.defaultFontSize = 18;
+    Chart.defaults.global.defaultFontColor = '#777';
+
+    let massPopChart = new Chart(myChart, {
+        type: 'pie', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
+        data: {
+            labels: departments,//['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
+            datasets: [{
+                label: 'Empleyees Per Department',
+                data: EmpsPerDepratment ,
+                //backgroundColor:'green',
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.6)',
+                    'rgba(54, 162, 235, 0.6)',
+                    'rgba(255, 206, 86, 0.6)',
+                    'rgba(75, 192, 192, 0.6)',
+                    'rgba(153, 102, 255, 0.6)',
+                    'rgba(255, 159, 64, 0.6)',
+                    'rgba(255, 99, 132, 0.6)'
+                ],
+                borderWidth: 1,
+                borderColor: '#777',
+                hoverBorderWidth: 3,
+                hoverBorderColor: '#000'
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Department Employees Statistics',
+                fontSize: 25
+            },
+            legend: {
+                display: true,
+                position: 'bottom',
+                labels: {
+                    fontColor: '#000'
+                }
+            },
+            layout: {
+                padding: {
+                    left: 50,
+                    right: 0,
+                    bottom: 0,
+                    top: 0
+                }
+            },
+            tooltips: {
+                enabled: true
+            }
+        }
+    });
+}
