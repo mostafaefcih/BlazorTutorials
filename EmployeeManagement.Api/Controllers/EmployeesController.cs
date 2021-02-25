fda134dbe9f7b3011104e98923e413dd003f1f6e
Mostafa-Eid <Mostafa-Eid@DESKTOP-6MU528G>
2021-02-25 15:01:23 +0200

    using Rotativa to convert html to pdf

--- EmployeeManagement.Api/Controllers/EmployeesController.cs
+++ EmployeeManagement.Api/Controllers/EmployeesController.cs
@@ -10,8 +10,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace EmployeeManagement.Api.Controllers
---
+++ EmployeeManagement.Web/Helpers/GeneratePdf.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Helpers
+{
+    public class GeneratePdf
+    {
+        private string Url;
+        public GeneratePdf(string _url)
+        {
+            this.Url = _url;
+        }
+        public byte[] GetPdf()
+        {
+            var switchs = $"-q {Url} -";
+            string rotativePath =
+                Path.Combine(Directory.GetCurrentDirectory(), "wwwroot",
+                "Rotativa",
+                "wkhtmltopdf.exe"
+                );
+            using (var proc = new Process())
+            {
+                try
+                {
+                    proc.StartInfo = new ProcessStartInfo()
+                    {
+                        FileName = rotativePath,
+                        Arguments = switchs,
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        RedirectStandardInput = true,
+                        RedirectStandardError = true,
+                        CreateNoWindow = true,
+                    };
+                    proc.Start();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                using (var ms = new MemoryStream())
+                {
+                    proc.StandardOutput.BaseStream.CopyTo(ms);
+                    return ms.ToArray();
+                }
+
+            }
+        }
+
+    }
+}
